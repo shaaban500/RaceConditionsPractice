b3e629088900b092a84c279048f6962a78e0f344
shaaban500 <moshaaban500@gmail.com>
2022-09-12 00:07:15 +0200

    Add project files.

---
+++ RaceConditionsPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿int count = 0;
+object pblock = new object();
+void doWork()
+{
+    for (int i = 0; i < 1000000; i++)
+    {
+        lock (pblock)
+        {
+            count++;
+        }
+    }
+}
+
+
+
+Thread t1 = new Thread(doWork);
+Thread t2 = new Thread(doWork);
+t1.Start();
+t2.Start();
+t1.Join();
+t2.Join();
+
+Console.WriteLine(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
